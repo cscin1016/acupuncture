adef883bc4d4adfefbed0ca47d68d9f141a29f5c
陈双超 <915893620@qq.com>
2015-05-12 11:04:07 +0800

    Initial Commit

---
+++ acupuncture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  acupuncture
+//
+//  Created by 陈双超 on 15/5/12.
+//  Copyright (c) 2015年 陈双超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ acupuncture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  acupuncture
+//
+//  Created by 陈双超 on 15/5/12.
+//  Copyright (c) 2015年 陈双超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
